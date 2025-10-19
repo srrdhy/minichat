eb920990c9754b57a743f7624e8e608648d04ef0
srrdhy <srrdhy@gmail.com>
2025-10-19 21:58:53 +0800

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,2 +1,2 @@
 // >>> Set your server URL here after you deploy the server (e.g. https://minichat-xyz.onrender.com)
-window.SERVER_URL = "http://localhost:4000";
+window.SERVER_URL = "https://minichat-b7fp.onrender.com";
